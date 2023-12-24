40e2ad025b7ec7b078e34e30da2d515c22ead41e
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-24 23:24:20 +0100

    Initial commit

---
+++ lib/blackbird.ex
@@ -0,0 +1,16 @@
+defmodule Blackbird do
+  @moduledoc """
+  Documentation for `Blackbird`.
+  """
+
+  @doc """
+  I Combinator
+
+  ## Examples
+
+      iex> Blackbird.i(12)
+      12
+
+  """
+  def i(x), do: x
+end
---
+++ test/blackbird_test.exs
@@ -0,0 +1,4 @@
+defmodule BlackbirdTest do
+  use ExUnit.Case
+  doctest Blackbird
+end
