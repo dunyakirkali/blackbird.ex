b09a8260cbdc29a1deac5af33dfb78bee0136375
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-24 23:26:23 +0100

    Add i, k and ki combinators

--- lib/blackbird.ex
+++ lib/blackbird.ex
@@ -13,4 +13,26 @@ defmodule Blackbird do
 
   """
   def i(x), do: x
+
+  @doc """
+  K Combinator
+
+  ## Examples
+
+      iex> Blackbird.k(12, 13)
+      12
+
+  """
+  def k(x, _), do: x
+
+  @doc """
+  KI Combinator
+
+  ## Examples
+
+      iex> Blackbird.ki(12, 13)
+      13
+
+  """
+  def ki(_, y), do: y
 end
